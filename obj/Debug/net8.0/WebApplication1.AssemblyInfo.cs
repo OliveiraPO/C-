bbd523cb9a78a82ba8091bfcefa2c6861a2f5ddb
Vinicius Paulo de Oliveira <viniciusp.oliveirapo@gmail.com>
2024-06-24 23:45:50 -0300

    atualizado

--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72fde9a4ada983638ccdcb5005f5ed852788b3a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d0fb2509aa2e7f05b43c253dc18b92ee580f45b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
